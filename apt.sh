799c0134e2944d65e7e8efe6d26d8b9615a943ae
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-02 11:03:24 -0300

    curl

--- apt.sh
+++ apt.sh
@@ -1,7 +1,8 @@
-sudo apt-get install git
+sudo apt-get install git curl
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install alsa-utils tmux gnuplot curl ktorrent lm-sensors wine nmap wget geany codeblocks vlc wireshark lua5.1 python3.4 netbeans htop kile eclipe* libreoffice 
+sudo apt-get install alsa-utils tmux gnuplot curl ktorrent lm-sensors wine
+sudo apt-get install nmap wget geany codeblocks vlc wireshark lua5.1 python3.4 netbeans htop kile eclipe* libreoffice
 sudo apt-get install unzip p7zip feh links python-opengl vim blender mysql-server mysql-workbench
 git clone https://github.com/tony/tmux-config.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
