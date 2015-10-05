9e9001a4b5477adf52f36ae5e7247578495cef40
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-05 10:44:23 -0300

    yum

--- yum/yum.sh
+++ yum/yum.sh
@@ -1,1 +1,11 @@
-yum
+#yum
+sudo yum update
+sudo yum upgrade
+sudo yum install alsa-utils tmux gnuplot curl ktorrent lm-sensors wine
+sudo yum install nmap wget geany codeblocks vlc wireshark lua5.1 python3.4 netbeans htop kile eclipe* libreoffice
+sudo yum install unzip p7zip feh links python-opengl vim blender mysql-server mysql-workbench
+sudo yum install gparted gimp lynx
+cd
+git clone https://github.com/tony/tmux-config.git ~/.tmux
+ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+curl http://j.mp/spf13-vim3 -L -o - | sh
