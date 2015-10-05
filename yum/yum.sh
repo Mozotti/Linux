b80a7af9bec9911cd470b336dc39eeace68f6427
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-05 11:17:55 -0300

    yum

--- yum/yum.sh
+++ yum/yum.sh
@@ -5,6 +5,8 @@ sudo yum install alsa-utils tmux gnuplot curl ktorrent lm-sensors wine
 sudo yum install nmap wget geany codeblocks vlc wireshark lua5.1 python3.4 netbeans htop kile eclipe* libreoffice
 sudo yum install unzip p7zip feh links python-opengl vim blender mysql-server mysql-workbench
 sudo yum install gparted gimp lynx
+sudo yum install gcc smplayer tatom bison flex man dhcpcd cowsay shotwell supertux gedit rhythmbox evince transmission qbittorrent
+sudo yum install openssh
 cd
 git clone https://github.com/tony/tmux-config.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
