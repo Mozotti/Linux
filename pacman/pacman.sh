a1c761f42a956b7a9b73bf461046dacac71bdc48
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-05 11:28:10 -0300

    pacman

--- pacman/pacman.sh
+++ pacman/pacman.sh
@@ -1,1 +1,9 @@
-pacman
+#oarcnab
+sudo pacman -Syyu
+sudo pacman -S alsa-utils tmux gnuplot xorg-xrandr shotwell pinta postgresql gedit git curl rhythmbox bashee evince gparted qbittorrent ktorrent trasmission lm_sensors wine supertux nmap wget irssi geany codeblocks vlc smplayer wireshark lua  python netbeans gcc htop kile gimp libreoffice eclipse gthumb conky firefox nano thunderbird feh zip unzip p7zip synaptics make htop links lynx gtk-engines lxappearances lm_sensors phonon-qt5-vlc totem totem-plparser lashplugin pgadmin3  postgresql mysql imagemagick scrot wine skype steam dnsutils lvm2 python-opengl
+python2-opengl gambas3-gb-qt4-opengl coin gliv glitz glfw glew glee gl2ps perl vim git mesa-demos lib32-mesa-demos blender mesa-libgl lib32-mesa-libgl unrar freeglut mesa mesa-demos openal glew awesome xorg-* pygame openssh
+sudo pacman -Syyu
+cd
+git clone https://github.com/tony/tmux-config.git ~/.tmux
+ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
+curl http://j.mp/spf13-vim3 -L -o - | sh
