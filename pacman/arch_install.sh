ede6c868ea76787bb8d5a33232a80ae2ba937b08
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-09 19:54:09 +0000

    arch install full

--- pacman/arch_install.sh
+++ pacman/arch_install.sh
@@ -1,22 +1,49 @@
+#durante a instação
 sudo fdisk -l
+cd ..
 sudo mount /dev/sda* /mnt
-sudo ip link set dev address ::::
+sudo ip link set dev eth0 address ::::
+#or ip link set dev PLACA addr
+# PLACA = ip addr 
 cd etc/pacman.d/
 vi mirrorlist | less
+#install 
 pacstrap /mnt base
 genfstab -p /mnt >> /mnt/etc/fstab
+#root create user here too
 arch-chroot /mnt
 echo computer_name > /etc/hostname
 ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 mkinitcpio -p linux
 passwd
+#new user 
+useradd -m -G wheel -s /bin/bash archie
+#erro try
+#useradd -m -g users -G wheel archie
+cd /home/ 
+#chmod g+s our_shared_directory
+chmod +x user_diretory/
+chown user_diretory/ 
+#verificar
+cd /home/ && ls -a
+exit
 #grub already install 
 #sudo update-grub
 #other linux partition
 
+#reboot
 cd home/name_user/
+sudo pacman -S reflector 
+reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist 
+sudo pacman -Syyu sudo vim git curl wget openssh htop lm_sensors links lynx
 vim ~/.xinitrc
 	exec awesome
-sudo pacman -S xorg-xinit
+sudo pacman -S xorg-xinit xorg xterm dialog awesome mesa gparted
+sudo pacman -Syyu alsa-utils tmux gnuplot xorg-xrandr shotwell gedit evince libreoffice geany
+#goto
+sudo su 
+cd /etc/pacman.conf 
+#remove # from multilib
+sudo pacman -S netbeans wine gcc gimp conky acpi firefox zip unzip make rhythmbox blender nmap irssi vlc smplayer lua wireshark
 #https://wiki.archlinux.org/index.php/Installation_guide_%28Portugu%C3%AAs%29
 #https://wiki.archlinux.org/index.php/Installation_guide
---
+++ pacman/install_mysql.sh
@@ -0,0 +1,6 @@
+sudo pacman -Syyu
+sudo pacman -S lidbda-mysql mysql-workbench mysql 
+sudo systemctl start mysqld
+sudo mysql_secure_installation
+#https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-arch-linux
+
