d7295742be1b6d2aa05ad86a58132f2183a2435d
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-15 16:06:40 -0300

    sdaji

--- udesc/udesc.sh
+++ udesc/udesc.sh
@@ -1,8 +1,9 @@
 su - suporte
 #B@gue77e
-sudo apt-get install tmux
+sudo apt-get install tmux curl
 exit
 git clone https://github.com/tony/tmux-config.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
 exit
 #rw init=/bin/bash
+curl http://j.mp/spf13-vim3 -L -o - | sh
