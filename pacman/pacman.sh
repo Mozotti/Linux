0ce33d3e2dbba3ef8ba66a34d8fe575c2f5a3f96
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-29 13:47:17 -0200

    comp

--- pacman/pacman.sh
+++ pacman/pacman.sh
@@ -1,5 +1,6 @@
 #oarcnab
 sudo pacman -Syyu
+sudo pacman -S sudo
 sudo pacman -S alsa-utils tmux gnuplot xorg-xrandr shotwell pinta postgresql gedit git curl rhythmbox bashee evince gparted qbittorrent ktorrent trasmission lm_sensors wine supertux nmap wget irssi geany codeblocks vlc smplayer wireshark lua  python netbeans gcc htop kile gimp libreoffice eclipse gthumb conky firefox nano thunderbird feh zip unzip p7zip synaptics make htop links lynx gtk-engines lxappearances lm_sensors phonon-qt5-vlc totem totem-plparser lashplugin pgadmin3  postgresql mysql imagemagick scrot wine skype steam dnsutils lvm2 python-opengl
 python2-opengl gambas3-gb-qt4-opengl coin gliv glitz glfw glew glee gl2ps perl vim git mesa-demos lib32-mesa-demos blender mesa-libgl lib32-mesa-libgl unrar freeglut mesa mesa-demos openal glew awesome xorg-* pygame openssh
 sudo pacman -Syyu
@@ -7,4 +8,4 @@ cd
 git clone https://github.com/tony/tmux-config.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
 curl http://j.mp/spf13-vim3 -L -o - | sh
-sudo pacman -S mplayer wireshark sudo
+sudo pacman -S mplayer wireshark bison flex
