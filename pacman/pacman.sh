68f8335deef7f28ded8eb1147dfd7d9ca370c2d2
h31nr1ch <tiagoheinrich1995@gmail.com>
2015-10-31 19:32:48 +0000

    sdasd

--- pacman/pacman.sh
+++ pacman/pacman.sh
@@ -8,4 +8,4 @@ cd
 git clone https://github.com/tony/tmux-config.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
 curl http://j.mp/spf13-vim3 -L -o - | sh
-sudo pacman -S mplayer wireshark bison flex
+sudo pacman -S mplayer wireshark bison flex acpi
