8f268a113b52b439b623fd3569d072f1cd5bf381
h31nr1ch <tiagoheinrich1995@gmail.com>
2016-08-29 13:57:42 +0000

    install atom qt5

---
+++ udesc/install_basic.sh
@@ -0,0 +1,9 @@
+#basic install commands
+sudo apt-get update
+sudo apt-get install tmux lynx links
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install atom
+wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
+chmod +x qt-opensource-linux-x64-5.7.0.run
+./qt-opensource-linux-x64-5.7.0.run
