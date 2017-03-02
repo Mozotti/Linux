782c2292822aa44ec9aa308b9475c6011a77202b
h31nr1ch <tiagoheinrich1995@gmail.com>
2017-03-02 08:20:27 -0300

    fixx

--- pacman/pacman.sh
+++ pacman/pacman.sh
@@ -1,9 +1,45 @@
 #oarcnab
 sudo pacman -Syyu
 sudo pacman -S sudo
-sudo pacman -S alsa-utils tmux gnuplot xorg-xrandr shotwell pinta postgresql gedit git curl rhythmbox bashee evince gparted qbittorrent ktorrent trasmission lm_sensors wine supertux nmap wget irssi geany codeblocks vlc smplayer wireshark lua  python netbeans gcc htop kile gimp libreoffice eclipse gthumb conky firefox nano thunderbird feh zip unzip p7zip synaptics make htop links lynx gtk-engines lxappearances lm_sensors phonon-qt5-vlc totem totem-plparser lashplugin pgadmin3  postgresql mysql imagemagick scrot wine skype steam dnsutils lvm2 python-opengl
-python2-opengl gambas3-gb-qt4-opengl coin gliv glitz glfw glew glee gl2ps perl vim git mesa-demos lib32-mesa-demos blender mesa-libgl lib32-mesa-libgl unrar freeglut mesa mesa-demos openal glew awesome xorg-* pygame openssh
+sudo pacman -S alsa-utils tmux gnuplot shotwell pinta postgresql gedit git curl rhythmbox bashee evince gparted qbittorrent ktorrent transmission-gtk lm_sensors
+sudo pacman -S wine supertux nmap wget irssi geany codeblocks vlc smplayer wireshark lua netbeans gcc htop kile gimp gthumb conky firefox nano
+sudo pacman -S thunderbird feh zip unzip p7zip synaptics make htop links lynx gtk-engines lxappearances lm_sensors phonon-qt5-vlc totem totem-plparser lashplugin
+sudo pacman -S pgadmin3  postgresql mysql imagemagick scrot dnsutils lvm2 python-opengl python2-opengl gambas3-gb-qt4-opengl
+sudo pacman -S coin gliv glitz glfw glew glee gl2ps perl vim mesa-demos lib32-mesa-demos blender mesa-libgl lib32-mesa-libgl unrar
+sudo pacman -S freeglut mesa mesa-demos openal glew pygame openssh
+#libreoffice BR
+sudo pacman -S libreoffice-fresh-pt-BR
+#install interface
+#sudo pacman -S awesome xorg-* xorg-xrandr
+sudo pacman -S cinelerra-cv  	boost-libs boost clang xz gnupg opencv julia ghc cabal-install clojure go python cmus moc php xterm ruby openmpi
+sudo pacman -S icoutils qt5 iotop atom texmaker eclipse-common calibre emacs
+#virtual box ##warning
+sudo pacman -S virtualbox
+#MELHOR BAIXAR O FONTE ...
+#sudo pacman -S skype steam
 sudo pacman -Syyu
+#-------------------------------------------------------------------------------------
+#yaourt install and configure
+#sudo nano /etc/pacman.conf
+#[archlinuxfr]
+#SigLevel = Never
+#Server = http://repo.archlinux.fr/$arch
+#sudo pacman -Sy yaourt
+#OR USE THIS
+sudo pacman -S --needed base-devel yajl
+git clone https://aur.archlinux.org/package-query.git
+cd package-query/cd package-query/
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt/
+makepkg -si
+cd ..
+sudo rm -dR yaourt/ package-query/
+#-------------------------------------------------------------------------------------
+#sublime for arch
+#sudo pacman -Sy base-devel
+#yaourt sublime-text-dev
 cd
 git clone https://github.com/tony/tmux-config.git ~/.tmux
 ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
